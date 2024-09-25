e5dac4d4fdf561946be9b042c726a751cde2ce8c
Gyarmati Csanád <gyarmati.csanad@burg.lo.hu>
2024-09-25 09:28:21 +0200

    osszes class letrehozva tablaknak

--- Foglalas.cs
+++ Foglalas.cs
@@ -8,5 +8,24 @@ namespace szepseg
 {
     internal class Foglalas
     {
+        int foglalasID, szolgaltatasID, dolgozoID, ugyfelID;
+        DateTime foglalasStart, foglalasEnd;
+
+        public Foglalas(int foglalasID, int szolgaltatasID, int dolgozoID, int ugyfelID, DateTime foglalasStart, DateTime foglalasEnd)
+        {
+            this.foglalasID = foglalasID;
+            this.szolgaltatasID = szolgaltatasID;
+            this.dolgozoID = dolgozoID;
+            this.ugyfelID = ugyfelID;
+            this.foglalasStart = foglalasStart;
+            this.foglalasEnd = foglalasEnd;
+        }
+
+        public int FoglalasID { get => foglalasID; set => foglalasID = value; }
+        public int SzolgaltatasID { get => szolgaltatasID; set => szolgaltatasID = value; }
+        public int DolgozoID { get => dolgozoID; set => dolgozoID = value; }
+        public int UgyfelID { get => ugyfelID; set => ugyfelID = value; }
+        public DateTime FoglalasStart { get => foglalasStart; set => foglalasStart = value; }
+        public DateTime FoglalasEnd { get => foglalasEnd; set => foglalasEnd = value; }
     }
 }
