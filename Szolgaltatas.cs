0b984e6b4a130779443e59bdfa20d0a27842e8e8
Gyarmati Csanád <gyarmati.csanad@burg.lo.hu>
2024-09-25 09:27:37 +0200

    classok letrehozasa

---
+++ Szolgaltatas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szepseg
+{
+    internal class Szolgaltatas
+    {
+        int szolgaltatasID, szolgaltatasAr;
+        string szolgaltatasKategoria;
+        DateTime szolgaltatasIdotartalma;
+
+        public Szolgaltatas(int szolgaltatasID, int szolgaltatasAr, string szolgaltatasKategoria, DateTime szolgaltatasIdotartalma)
+        {
+            this.szolgaltatasID = szolgaltatasID;
+            this.szolgaltatasAr = szolgaltatasAr;
+            this.szolgaltatasKategoria = szolgaltatasKategoria;
+            this.szolgaltatasIdotartalma = szolgaltatasIdotartalma;
+        }
+
+        public int SzolgaltatasID { get => szolgaltatasID; set => szolgaltatasID = value; }
+        public int SzolgaltatasAr { get => szolgaltatasAr; set => szolgaltatasAr = value; }
+        public string SzolgaltatasKategoria { get => szolgaltatasKategoria; set => szolgaltatasKategoria = value; }
+        public DateTime SzolgaltatasIdotartalma { get => szolgaltatasIdotartalma; set => szolgaltatasIdotartalma = value; }
+    }
+}
